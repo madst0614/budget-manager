45bfbc5dd67f6fdb2a2036ab42be8b86cad3ebed
sh <madst0614@gmail.com>
2023-11-10 18:31:19 +0900

    init: spring 추가

---
+++ src/main/java/wanted/n/budgetmanager/BudgetManagerApplication.java
@@ -0,0 +1,13 @@
+package wanted.n.budgetmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetManagerApplication.class, args);
+    }
+
+}
