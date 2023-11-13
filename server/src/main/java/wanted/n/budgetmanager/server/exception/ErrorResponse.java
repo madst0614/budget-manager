af5bd143b80470557db96aa3239516370df25f8a
sh <madst0614@gmail.com>
2023-11-13 22:51:39 +0900

    init: 예외처리 및 audit 추가

---
+++ server/src/main/java/wanted/n/budgetmanager/server/exception/ErrorResponse.java
@@ -0,0 +1,11 @@
+package wanted.n.budgetmanager.server.exception;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class ErrorResponse {
+    private ErrorCode errorCode;
+    private String message;
+}
