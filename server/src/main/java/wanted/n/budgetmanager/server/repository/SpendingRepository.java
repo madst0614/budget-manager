dcd0f3498b295efe25ad9a4c115d90a3ba4cd289
sh <madst0614@gmail.com>
2023-11-23 03:16:55 +0900

    add: @Repository

--- server/src/main/java/wanted/n/budgetmanager/server/repository/SpendingRepository.java
+++ server/src/main/java/wanted/n/budgetmanager/server/repository/SpendingRepository.java
@@ -2,8 +2,10 @@ package wanted.n.budgetmanager.server.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import wanted.n.budgetmanager.server.domain.Spending;
 import wanted.n.budgetmanager.server.repository.q.SpendingQRepository;
 
+@Repository
 public interface SpendingRepository extends JpaRepository<Spending, Long>, SpendingQRepository {
 }
